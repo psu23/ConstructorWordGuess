7674602d7df26e7266ff85808cfa08f96e8256b6
Patrick Urbankowski <purbankowski@gmail.com>
2020-06-14 20:09:34 -0400

    added initial code to Letter.js

---
+++ Letter.js
@@ -0,0 +1,23 @@
+class Letter {
+    constructor(character){
+        this.character = character;
+        this.guessed = false;
+    }
+
+    letterGuess() {
+        if (this.guessed) {
+            return this.character;
+        }
+        else {
+            return "- ";
+        }
+    }
+
+    matchGuess(letter){
+        if (this.character === letter) {
+            this.guessed = true;
+        }
+    }
+}
+
+module.exports = Letter;
